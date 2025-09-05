3e437bc5e03fc969d74db99a0736b44299787c02
jitesh-95 <1995sirohiss@gmail.com>
2025-09-06 01:51:43 +0530

    config file updated

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // tells Next.js to use the App Router in src/app
-  experimental: {
-    appDir: true,
-  },
-  reactStrictMode: true,
-};
+const nextConfig = {};
 
 export default nextConfig;
